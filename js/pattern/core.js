b055fc2203e37ec66a3e21f7eb9a18a5a68a5b5b
Kushikino School <18130182@kago.ed.jp>
2026-07-13 15:43:15 +0900

    Update core.js

--- js/pattern/core.js
+++ js/pattern/core.js
@@ -1,1 +1,59 @@
+// ======================================
+// PatternLock-v5
+// core.js
+// ======================================
 
+// ドット一覧
+const dots = [...document.querySelectorAll(".dot")];
+
+// SVG
+const svg = document.getElementById("patternCanvas");
+
+// メッセージ
+const message = document.getElementById("message");
+
+// ボタン
+const loginButton = document.getElementById("loginButton");
+const resetButton = document.getElementById("resetButton");
+const saveButton = document.getElementById("saveButton");
+
+// パターン
+let pattern = [];
+
+// 描画中か
+let drawing = false;
+
+// 仮線
+let currentLine = null;
+
+// --------------------
+// パターン取得
+// --------------------
+function getPattern() {
+    return [...pattern];
+}
+
+// --------------------
+// パターン追加
+// --------------------
+function addPattern(id) {
+
+    if (pattern.includes(id)) {
+        return false;
+    }
+
+    pattern.push(id);
+
+    return true;
+}
+
+// --------------------
+// パターン初期化
+// --------------------
+function resetPatternData() {
+
+    pattern = [];
+
+    drawing = false;
+
+}
